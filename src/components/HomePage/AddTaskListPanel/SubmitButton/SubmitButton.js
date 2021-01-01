cd0712cc7ef83b8aa66050bd4fc9d6d6cb89402f
unknown <alinh1803@gmail.com>
2021-01-01 23:26:23 +0700

    Updates

--- src/components/HomePage/AddTaskListPanel/SubmitButton/SubmitButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './SubmitButton.css';
-
-const submitButton = (props) => (
-    <p className="AddButtonSubmit" onClick={props.click}>Submit</p>
-
-)
-
-export default submitButton;
